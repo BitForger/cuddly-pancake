a08b35b467ad3063e563c739d375f246c9959369
Noah Kovacs <kovacsn66@gmail.com>
2020-01-23 20:40:50 -0500

    feat(todo): add update route, minor tweaks to responses
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- src/controllers/todo/todo.controller.ts
+++ src/controllers/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, HttpStatus, InternalServerErrorException, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpStatus, InternalServerErrorException, Param, Patch, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ITodo } from '../../models/todo.model';
@@ -9,7 +9,7 @@ export class TodoController {
   }
 
   @Post('')
-  protected async create(@Body('document') doc: ITodo): Promise<{ status: number, id?: string }> {
+  protected async create(@Body('document') doc: ITodo): Promise<{ status: number, id?: string, document: ITodo }> {
     // if required fields are not in object reject it
     if (!('name' in doc) || !('priority' in doc)) {
       throw new BadRequestException({status: HttpStatus.BAD_REQUEST});
@@ -20,7 +20,7 @@ export class TodoController {
 
     // save it
     if (await todo.save()) {
-      return {status: HttpStatus.CREATED, id: todo._id};
+      return {status: HttpStatus.CREATED, id: todo._id, document: todo};
     }
     // save failed
     throw new BadRequestException({status: HttpStatus.BAD_REQUEST});
@@ -45,6 +45,18 @@ export class TodoController {
       return docs;
     } catch (e) {
       throw new InternalServerErrorException({status: HttpStatus.INTERNAL_SERVER_ERROR, message: e.message});
+    }
+  }
+
+  @Patch('')
+  protected async update(@Body('document') doc: ITodo) {
+    try {
+      const result = await this.todoModel.findOne({_id: doc._id}).exec();
+      result.priority = doc.priority;
+      await result.save();
+      return {status: HttpStatus.OK, id: result._id, document: result};
+    } catch (e) {
+      throw new InternalServerErrorException({status: HttpStatus.INTERNAL_SERVER_ERROR});
     }
   }
 }
